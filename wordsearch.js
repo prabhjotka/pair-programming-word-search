3c22b9819fa5647584a31090c4874bc94e2fa8e9
Prabh <sonukaur68@gmail.com>
2023-09-26 16:37:16 -0600

    pariprogreammimg added

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+const transpose = function (mat) {
+ 
+    const result = [];
+  
+    for (let i = 0; i < mat[0].length; i++) {
+      const col = []
+      for (let j = 0; j < mat.length; j++) {
+        col.push(mat[j][i]);
+      }
+      result.push(col)
+    }
+  
+    return result
+  };
+  const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    const lengthle = letters.length;
+    if (lengthle === 0) {
+      return false;
+    }
+  
+    for (let l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+  
+    for (let l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+  
+    return false;
+  };
+  
+  module.exports = wordSearch;
